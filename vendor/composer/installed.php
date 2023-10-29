a79188d0f93049e2c3cbf1562b34b5a0c2071b7c
Peter Gerbenzon <peter@ylt.nl>
2023-10-29 14:14:12 +0100

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dttmultimedia/web_backend_test_catering_api',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5521c41c58b82504fead3d04fd25053f19456865',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bramus/router' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'reference' => '55657b76da8a0a509250fb55b9dd24e1aa237eba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bramus/router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'dttmultimedia/web_backend_test_catering_api' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5521c41c58b82504fead3d04fd25053f19456865',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
